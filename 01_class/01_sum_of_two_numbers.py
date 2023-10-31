150945b69e21b8cf2a82979f4f9335281ba2ff91
Ilia Orlov <2x2.4@mail.ru>
2023-10-31 23:21:39 +0100

    Class 01, task 01

---
+++ 01_class/01_sum_of_two_numbers.py
@@ -0,0 +1,8 @@
+# You have a non-empty array with distinct integers and an integer representing target sum.
+# Write a function that check if there are two numbers in the array sum up to the target sum.
+# If there are then return these two numbers.
+# If there aren't then return None.
+
+
+def two_num_sum(arr, target_sum):
+    pass
