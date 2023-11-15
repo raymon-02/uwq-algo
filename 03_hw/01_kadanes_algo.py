0c99d9934f5948c586281e63187b1a2d71d108dc
Ilia Orlov <2x2.4@mail.ru>
2023-11-15 10:46:37 +0100

    Class 3

--- 03_hw/01_kadanes_algo.py
+++ 03_hw/01_kadanes_algo.py
@@ -2,6 +2,11 @@
 # and returns the maximum sum that can be constructed
 # by summing up all the integers in a non-empty subarray of the input array.
 # A subarray must only contain adjacent numbers.
+#
+# Example
+# Input: [1, 4, -10, 2, 3]
+# Output: 5
+# Subarray will be [1, 4]
 
 def kadanes_algo(arr):
     pass
