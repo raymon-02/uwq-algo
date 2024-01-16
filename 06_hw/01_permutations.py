33292a2663e7a3487af39d76734cab35e7c8fe48
Ilia Orlov <2x2.4@mail.ru>
2024-01-16 23:41:21 +0100

    Homework 6

---
+++ 06_hw/01_permutations.py
@@ -0,0 +1,10 @@
+# Write a function that takes an array of unique integers
+# and returns all possible permutations of the array.
+#
+# Example
+# Input: [1, 2, 3]
+# Output: [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+
+
+def permutations(arr):
+    pass
---
+++ 06_hw/02_dialer_mnemonics.py
@@ -0,0 +1,28 @@
+# Write a function that takes string of digits
+# and returns all possible mnemonics
+# associated with keypad of mobile phone.
+#
+# Each button of keypad contains digit and letters like:
+# 1 has no letters
+# 2 has 'a', 'b', 'c'
+# 3 has 'd', 'e', 'f'
+# 4 has 'g', 'h', 'i'
+# 5 has 'j', 'k', 'l'
+# 6 has 'm', 'n', 'o'
+# 7 has 'p', 'q', 'r', 's'
+# 8 has 't', 'u', 'v'
+# 9 has 'w', 'x', 'y', 'z'
+# 0 has no letters
+#
+# So input string "234" can be presented
+# like "aei" or "bfg" or "cdh" and so on.
+#
+# If digit has no letters then it should be presented as is.
+#
+# Example
+# Input: "290"
+# Output: ["aw0", "ax0", "ay0", "az0", "bw0", "bx0", "by0", "bz0", "cw0", "cx0", "cy0", "cz0"]
+
+
+def dialer_mnemonics(s):
+    pass
