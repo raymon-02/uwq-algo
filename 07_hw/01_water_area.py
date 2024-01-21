5384eb37cfd509981abf1e4350cb4f84fc4bde51
Ilia Orlov <2x2.4@mail.ru>
2024-01-21 19:10:01 +0100

    Homework 7

---
+++ 07_hw/01_water_area.py
@@ -0,0 +1,21 @@
+# There are n pillars with different heights and width of 1.
+# Water pours over all the pillars.
+#
+# Write a function that takes an array of non-negative integers
+# representing the heights of the pillars
+# and returns an amount of water trapped between pillars.
+#
+# Example
+# Input: [0, 3, 2, 1, 4, 1, 2]
+# Output: 4
+# Image with pillars where | is part of a pillar and X means water.
+# There are 4 X.
+#
+#     |
+#  |XX|
+#  ||X|X|
+# _||||||
+
+
+def water_area(heights):
+    pass
---
+++ 07_hw/02_max_profit_transactions.py
@@ -0,0 +1,18 @@
+# There is an array `prices` of positive integers
+# representing the prices of single stock for n days.
+#
+# Write a functions that takes an array `prices` and positive k
+# and returns maximum profit that is possible to make
+# using k transactions and holding one share of the stock at a time.
+# One transaction means buy stock on one day and sell it on another.
+#
+# Example
+# Input: [5, 11, 3, 50, 60, 90], 2
+# Output: 93
+# Buy 5, sell 11 => profit is 6
+# Buy 3, sell 90 => profit is 87
+# Total profit is 6 + 87 = 93
+
+
+def max_profit(prices, k):
+    pass
