f5b8202340bf934e4c795da0b0bc80793458d796
Ilia Orlov <2x2.4@mail.ru>
2024-02-21 00:28:22 +0100

    HW 09 -- solution

--- 09_hw/01_way_number_for_change.py
+++ 09_hw/01_way_number_for_change.py
@@ -16,5 +16,77 @@
 # Total ways 1 + 1 = 2
 
 
+def way_coins_rec(n, denoms):
+    def rec(rest, i):
+        if rest < 0 or i == len(denoms):
+            return 0
+        if rest == 0:
+            return 1
+        take = rec(rest - denoms[i], i)
+        leave = rec(rest, i + 1)
+        return take + leave
+
+    return rec(n, 0)
+
+
+# O((N + D)! / N! / D!) -- time
+# O(max(N, D)) -- space
+
+# Factorial time without cache.
+
+
+def way_coins_rec_cache(n, denoms):
+    cache = {}
+
+    def rec(rest, i):
+        if (rest, i) in cache:
+            return cache[(rest, i)]
+        if rest < 0 or i == len(denoms):
+            return 0
+        if rest == 0:
+            return 1
+        take = rec(rest - denoms[i], i)
+        leave = rec(rest, i + 1)
+        res = take + leave
+        cache[(rest, i)] = res
+        return res
+
+    return rec(n, 0)
+
+
+# O(N * D) -- time
+# O(N * D) -- space
+
+# Still recursive. Stack problem.
+
+
+def way_coins_grid(n, denoms):
+    grid = [[0] * len(denoms) for _ in range(n + 1)]
+    grid[0] = [1] * len(denoms)
+    for i in range(1, n + 1):
+        for j in range(len(denoms)):
+            take = grid[i - denoms[j]][j] if i - denoms[j] >= 0 else 0
+            leave = grid[i][j - 1] if j > 0 else 0
+            grid[i][j] = take + leave
+
+    return grid[-1][-1]
+
+
+# O(N * D) -- time
+# O(N * D) -- space
+
+# No need to have full grid.
+
+
 def way_coins(n, denoms):
-    pass
+    grid = [0] * (n + 1)
+    grid[0] = 1
+    for j in range(len(denoms)):
+        for i in range(1, n + 1):
+            if i >= denoms[j]:
+                grid[i] += grid[i - denoms[j]]
+
+    return grid[-1]
+
+# O(N * D) -- time
+# O(N) -- space
--- 09_hw/02_longest_common_subsequence.py
+++ 09_hw/02_longest_common_subsequence.py
@@ -7,4 +7,31 @@
 
 
 def longest_common_subsequence(str1, str2):
-    pass
+    str1 = "." + str1
+    str2 = "." + str2
+    grid = [[0] * len(str2) for _ in range(len(str1))]
+
+    for i in range(1, len(str1)):
+        for j in range(1, len(str2)):
+            if str1[i] == str2[j]:
+                grid[i][j] = grid[i - 1][j - 1] + 1
+            else:
+                grid[i][j] = max(grid[i - 1][j], grid[i][j - 1])
+
+    result = []
+    i = len(str1) - 1
+    j = len(str2) - 1
+    while i != 0 and j != 0:
+        if grid[i][j] == grid[i - 1][j]:
+            i -= 1
+        elif grid[i][j] == grid[i][j - 1]:
+            j -= 1
+        else:
+            result.append(str1[i])
+            i -= 1
+            j -= 1
+
+    return list(reversed(result))
+
+# O(N * M) -- time
+# O(N * M) -- space
