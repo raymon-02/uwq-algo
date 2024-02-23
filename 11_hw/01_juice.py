ef737fdefc8451257c36d6ae3b7d79d7323fbe4d
Ilia Orlov <2x2.4@mail.ru>
2024-02-23 15:49:33 +0100

    HW 11

---
+++ 11_hw/01_juice.py
@@ -0,0 +1,35 @@
+# Write a function that takes prices of juice
+# and returns the indices of prices from which
+# the maximum profit can be constructed.
+# Each price under index i represents i amount of juice
+# and the maximum amount of juice is n - 1
+# where n is a length of prices.
+#
+# Each amount of juice can be used multiple times.
+#
+# At index 0 in prices there will be always 0 value
+# as price for 0 amount of juice is 0.
+#
+# Example
+# Input: [0, 1, 3, 2]
+# Output: [1, 2]
+# The maximum amount of juice is 3 as length of prices is 4.
+# 1 amount of juice has price 1.
+# 2 amount of juice has price 3.
+# In sum it is 1+2=3 amount of juice with profit 1+3=4.
+# This is the maximum profit that can be constructed
+# having 3 amount of juice in total.
+#
+# Example
+# Input: [0, 2, 3]
+# Output: [1, 1]
+# The maximum amount of juice is 2 as length of prices is 3.
+# 1 amount of juice has price 2.
+# We can use 1 amount of juice multiple times.
+# In sum it is 1+1=2 amount of juice with profit 2+2=4.
+# This is the maximum profit that can be constructed
+# having 2 amount of juice in total.
+
+
+def juice(prices):
+    pass
---
+++ 11_hw/02_task_assigment.py
@@ -0,0 +1,21 @@
+# There are integer k representing number of workers
+# and an array of positive integers representing duration of tasks.
+# The length of the array is 2k. Each worker must complete 2 tasks.
+#
+# Write a function that takes k and the array
+# and returns an array representing
+# the optimal assignment of tasks to each worker.
+# The length of the returned array is k.
+# Each element in the returned array is an array ot two elements
+# where each of two elements are indices of tasks
+# that are assigned on worker.
+#
+# Example
+# Input: 3, [1, 3, 5, 3, 1, 4]
+# Output: [[0, 2], [4, 5], [1, 3]]
+# Tasks 0 and 2 are assigned on worker 0 and will take 1+5=6.
+# Tasks 4 and 5 are assigned on worker 1 and will take 1+4=5.
+# Tasks 1 and 3 are assigned on worker 2 and will take 3+3=6.
+
+def task_assignment(k, tasks):
+    pass
