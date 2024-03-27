28f8eff45c261ed143514d14202e7d1cfd0feb8c
Ilia Orlov <2x2.4@mail.ru>
2024-03-27 10:53:30 +0100

    HW 14

---
+++ 14_hw/01_different_substrings.py
@@ -0,0 +1,10 @@
+# Write a function that takes a string
+# and returns the number of different substring in the string.
+#
+# Example
+# Input: "abbabba"
+# Output: 17
+
+
+def different_substrings(string):
+    pass
---
+++ 14_hw/02_knuth_morris_pratt.py
@@ -0,0 +1,11 @@
+# Write a function that takes a string and a pattern
+# and returns if the pattern is a substring of the string.
+# The function should implement Knuth-Morris-Pratt algorithm.
+#
+# Example
+# Input: "abbcdef", "bbcd"
+# Output: True
+
+
+def knuth_morris_pratt_algo(string, pattern):
+    pass
