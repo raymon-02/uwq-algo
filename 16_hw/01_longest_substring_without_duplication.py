5e876edf6901aa7fa25f1a162adbcc506cfc39f7
Ilia Orlov <2x2.4@mail.ru>
2024-04-09 10:42:31 +0200

    HW 16

---
+++ 16_hw/01_longest_substring_without_duplication.py
@@ -0,0 +1,10 @@
+# Write a function that takes a string
+# and returns the longest substring without duplicate characters.
+#
+# Example
+# Input: "abcdbafec"
+# Output: "cdbafe"
+
+
+def longest_substring_without_duplication(string):
+    pass
---
+++ 16_hw/02_longest_balanced_substring.py
@@ -0,0 +1,10 @@
+# Write a function that takes a string consisting of parenthesis
+# and returns the longest substring consisting of balanced parenthesis.
+#
+# Example
+# Input: "(())()(()()))((())"
+# Output: 12
+
+
+def longest_balanced_substring(string):
+    pass
