c0958a9cf8f3f79f9cc1eebb452c83ff2fbade41
Ilia Orlov <2x2.4@mail.ru>
2024-04-27 14:47:24 +0200

    HW 16 -- solution

--- 16_hw/01_longest_substring_without_duplication.py
+++ 16_hw/01_longest_substring_without_duplication.py
@@ -6,5 +6,41 @@
 # Output: "cdbafe"
 
 
+def longest_substring_without_duplication_index(string):
+    cache = set()
+    l, r = 0, 0
+    res_l, res_r = 0, 0
+
+    while r < len(string):
+        while string[r] in cache:
+            cache.remove(string[l])
+            l += 1
+        cache.add(string[r])
+        r += 1
+
+        if r - l > res_r - res_l:
+            res_l, res_r = l, r
+
+    return string[res_l: res_r]
+
+
+# O(N) -- time
+# O(N) -- space
+
+
 def longest_substring_without_duplication(string):
-    pass
+    cache = {}
+    l = 0
+    res_l, res_r = 0, 0
+
+    for i, char in enumerate(string):
+        if char in cache:
+            l = max(l, cache[char])
+        cache[char] = i + 1
+        if i - l + 1 > res_r - res_l:
+            res_l, res_r = l, i + 1
+
+    return string[res_l:res_r]
+
+# O(N) -- time
+# O(N) -- space
--- 16_hw/02_longest_balanced_substring.py
+++ 16_hw/02_longest_balanced_substring.py
@@ -6,5 +6,73 @@
 # Output: 12
 
 
+def longest_balanced_substring_stack_iter(string):
+    stack = []
+    cons = []
+    for i, char in enumerate(string):
+        if char == "(":
+            stack.append(i)
+        else:
+            if stack:
+                l = stack.pop()
+                while cons and cons[-1][0] > l:
+                    cons.pop()
+                cons.append((l, i))
+
+    result = 0
+    curr_l, curr_r = 0, -1
+    for i, (l, r) in enumerate(cons):
+        if curr_r + 1 < l:
+            result = max(result, curr_r - curr_l + 1)
+            curr_l, curr_r = l, r
+        elif curr_r + 1 == l:
+            curr_r = r
+    result = max(result, curr_r - curr_l + 1)
+
+    return result
+
+
+# O(N) -- time
+# O(N) -- space
+
+
+def longest_balanced_substring_stack(string):
+    stack = [-1]
+    result = 0
+    for i, char in enumerate(string):
+        if char == "(":
+            stack.append(i)
+        else:
+            stack.pop()
+            if stack:
+                result = max(result, i - stack[-1])
+            else:
+                stack.append(i)
+
+    return result
+
+
+# O(N) -- time
+# O(N) -- space
+
+
 def longest_balanced_substring(string):
-    pass
+    def count(reverse=False):
+        result = 0
+        s = reversed(string) if reverse else string
+        open_counter, close_counter = 0, 0
+        for char in s:
+            if char == "(":
+                open_counter += 1
+            else:
+                close_counter += 1
+            if open_counter == close_counter:
+                result = max(result, open_counter * 2)
+            elif close_counter > open_counter and not reverse or open_counter > close_counter and reverse:
+                open_counter, close_counter = 0, 0
+        return result
+
+    return max(count(), count(reverse=True))
+
+# O(N) -- time
+# O(1) -- space
