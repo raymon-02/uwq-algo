30a0a2a4c93c6f626c4741a676ac3110498c67dc
Ilia Orlov <2x2.4@mail.ru>
2024-06-12 01:31:23 +0200

    HW 19

---
+++ 19_hw/01_cycle_in_graph.py
@@ -0,0 +1,9 @@
+# Write a function that takes graph in adjacency format
+# and returns boolean representing if the graph contains a cycle.
+#
+# Example
+# Input: [[1, 2], [2], []]
+# Output: False
+
+def cycle_in_graph(graph):
+    pass
---
+++ 19_hw/02_job_order.py
@@ -0,0 +1,14 @@
+# There are list of integers representing jobs to complete
+# and list of pairs of integers where each pair represents job dependencies.
+# The second job in pair depends on the first one.
+# Write a function that takes these two arrays
+# and returns jobs in valid order to complete.
+# If there is no such order the function should return empty array.
+#
+# Example
+# Input: [1, 2, 3, 4], [(1, 2), (1, 3), (3, 2), (4, 2), (4, 3)]
+# Output: [4, 1, 3, 2]
+
+
+def job_order(jobs, deps):
+    pass
